30555a06bae33d26d2c8575a59bfb9b0d0b0ec5e
João Spinelli <jgsspinelli92@gmail.com>
2023-08-27 18:07:48 -0300

    chore: adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto do curso do Felipe Deschamps onde vou criar um clone do TabNews.
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 export default function Home() {
   return (
-  <div>
-    <img src="https://cinepop.com.br/wp-content/uploads/2022/10/thriller-3D.webp" />
+    <div>
+      <img src="https://cinepop.com.br/wp-content/uploads/2022/10/thriller-3D.webp" />
 
-    <h1>Bia eu to vivo, e vou te visitar de madrugada do dia 30/08!! se quiser me ver olhe debaixo da cama!</h1>
-  </div>
-  )
+      <h1>
+        Bia eu to vivo, e vou te visitar de madrugada do dia 30/08!! se quiser
+        me ver olhe debaixo da cama!
+      </h1>
+    </div>
+  );
 }
