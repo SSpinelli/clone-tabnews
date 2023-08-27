1668f6d17e620827af4cbf206d922ca6dd508afd
João Spinelli <jgsspinelli92@gmail.com>
2023-08-26 22:22:10 -0300

    feat: Conclui o desafio do Deschamps.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <h1>Teste</h1>
+  return (
+  <div>
+    <img src="https://cinepop.com.br/wp-content/uploads/2022/10/thriller-3D.webp" />
+
+    <h1>Bia eu to vivo, e vou te visitar de madrugada do dia 30/08!! se quiser me ver olhe debaixo da cama!</h1>
+  </div>
+  )
 }
